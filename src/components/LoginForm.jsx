bd21a88615d76e7341e2e35bbc791cdf814bd40f
Emmanuel Ezema <emmanuelezema6@gmail.com>
2023-02-01 23:00:45 +0100

    new commit

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,19 +1,36 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
-const LoginForm = ({ handleLogin, login, handleLoginChange, authStatus }) => {
+const LoginForm = ({ handleLogin, handleLoginChange, authStatus }) => {
+  const [showPword, setShowPword] = useState(false);
+
+  const handleClick = () => {
+    setShowPword(prev => !prev)
+  }
+
+  
   return (
       <form onSubmit={handleLogin} className="login full-w">
-        <p className="flex-row form-quest align-items-center full-w">
+        <div className="flex-row form-quest align-items-center full-w">
             <p className="form-text">Don't have an account? <span className="form-link"><Link to="/register">Register</Link></span></p>
             <h1 className="logo">cardStore</h1>
-        </p>
+        </div>
         <h2 className="title full-border full-w">Login</h2>
         <p id="error" className="bad full-w">
           {authStatus.toString()}
         </p>
         <div className="inputs flex-col">
-            <input type="text" name="username" id="username" placeholder="Username" value={login.username} onChange={handleLoginChange} />
-            <input type="password" name="password" id="password" placeholder="Password" value={login.password} onChange={handleLoginChange} />
+            <input type="text" name="username" id="username" placeholder="Username" onChange={handleLoginChange} />
+            <div className="pword_input flex-row align-items-flexend">
+                <input type={showPword ? 'text': 'password'} name="password" id="password" placeholder="Password" onChange={handleRegisterChange} />
+                <button className="change_input_type flex-row justify-content-center align-items-center" type="button" onClick={handleClick}>
+                {
+                  showPword ?
+                  <BiHide /> :
+                  <BiShow />
+                }
+              </button>
+            </div>
         </div>
         <button className="full-w">Log In</button>
     </form>
--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { MdCancel } from "react-icons/md"
 import { BsCheckCircleFill } from 'react-icons/bs'
+import { BiShow, BiHide } from 'react-icons/bi'
 
 const RegisterForm = ({ register, handleRegisterChange, handleRegister, pwordCheck, authStatus }) => {
   const [showPword, setShowPword] = useState(false)
@@ -12,42 +13,50 @@ const RegisterForm = ({ register, handleRegisterChange, handleRegister, pwordChe
 
   return (
     <form onSubmit={handleRegister} className="register full-w">
-          <p className="flex-row form-quest full-w align-items-center">
+          <div className="flex-row form-quest full-w align-items-center">
             <p className="form-text">Already Registered? <span className="form-link"><Link to="/login">Login</Link></span></p>
             <h1 className="logo">cardStore</h1>
-          </p>
+          </div>
           <h2 className="title full-border full-w">Create Sign In</h2>
           <div className="inputs flex-col">
             <p id="error" className="bad full-w">
               {authStatus}
             </p>
             <div className="names flex-row">
-              <input type="text" className="first_name" placeholder="First Name" name="first_name" value={register.names.first_name} />
-              <input type="text" className="middle_name" placeholder="Middle Name" name="middle_name" value={register.names.middle_name} />
-              <input type="text" className="last_name" placeholder="Last Name" name="last_name" value={register.names.last_name} />
+              <input type="text" className="first_name" placeholder="First Name" name="first_name" onChange={handleRegisterChange} />
+              <input type="text" className="middle_name" placeholder="Middle Name" name="middle_name" onChange={handleRegisterChange} />
+              <input type="text" className="last_name" placeholder="Last Name" name="last_name"onChange={handleRegisterChange} />
             </div>
 
             <div className="contact flex-row">
-              <input type="email" name="email" id="email" placeholder="Email" value={register.email} onChange={handleRegisterChange} />
-              <input type="tel" name="phone" id="phone" placeholder="Phone Number" value={register.phone} onChange={handleRegisterChange} />
+              <input type="email" name="email" id="email" placeholder="Email" onChange={handleRegisterChange} />
+              <input type="tel" name="phone" id="phone" placeholder="Phone Number" onChange={handleRegisterChange} />
             </div>
 
             <div className="bio relative flex-row align-items-center">
               <label htmlFor="dob">Date of Birth</label>
-              <input type="date" name="dob" id="dob" placeholder="Date of Birth" className="full-w" value={register.age} onChange={handleRegisterChange} />
-              <input type="text" name="address" id="address" placeholder="Address" value={register.address} onChange={handleRegisterChange} />
+              <input type="date" name="dob" id="dob" placeholder="Date of Birth" className="full-w" onChange={handleRegisterChange} />
+              <input type="text" name="address" id="address" placeholder="Address" onChange={handleRegisterChange} />
             </div>
 
             <div className="bank_info full-w flex-row">
-              <input type="text" name="bank" id="bank" placeholder="Bank" value={register.bank} onChange={handleRegisterChange} />
-              <input type="number" name="account_number" id="account_number" placeholder="Account Number" value={register.account_number} onChange={handleRegisterChange} />
+              <input type="text" name="bank" id="bank" placeholder="Bank" onChange={handleRegisterChange} />
+              <input type="number" name="account_number" id="account_number" placeholder="Account Number" onChange={handleRegisterChange} />
             </div>
             
-            <input type="text" name="username" id="username" placeholder="Username" value={register.username} onChange={handleRegisterChange} />
+            <input type="text" name="username" id="username" placeholder="Username" onChange={handleRegisterChange} />
             <div className="passwords full-w flex-row">
-              <input type={showPword ? 'text': 'password'} name="password" id="password" placeholder="Password" value={register.password} onChange={handleRegisterChange} />
-              <input type={showPword ? 'text': 'password'} name="confirmPword" id="cofirmPword" placeholder="Confirm Password" value={register.confirmPword} onChange={handleRegisterChange} /> 
-              <button type="button" onClick={handleClick}>Show Password</button>
+              <div className="pword_input flex-row align-items-flexend">
+                <input type={showPword ? 'text': 'password'} name="password" id="password" placeholder="Password" onChange={handleRegisterChange} />
+                <button className="change_input_type flex-row justify-content-center align-items-center" type="button" onClick={handleClick}>
+                  {
+                    showPword ?
+                    <BiHide /> :
+                    <BiShow />
+                  }
+                </button>
+              </div>
+              <input type={showPword ? 'text': 'password'} name="confirmPword" id="cofirmPword" placeholder="Confirm Password" onChange={handleRegisterChange} /> 
             </div>
              
             <p className="form-text">
@@ -55,12 +64,14 @@ const RegisterForm = ({ register, handleRegisterChange, handleRegister, pwordChe
                 pwordCheck && pwordCheck.split('\n').map(
                   (text, i) => (text === "All good") ?
                       <>
-                        <span key={i} className="flex-row align-items-center good"><i className="flex-row align-items-center justify-content-center"><BsCheckCircleFill /></i>{text}</span><br key={i} />
+                        <span key={i} className="flex-row align-items-center good">
+                          <i className="flex-row align-items-center justify-content-center"><BsCheckCircleFill /></i>{text}
+                        </span><br key={text} />
                       </> :
                       <>
-                        <span key={i} className="flex-row align-items-center bad"><i className="flex-row align-items-center justify-content-center">
-                          <MdCancel /></i>{text}
-                        </span><br key={i} />
+                        <span key={i} className="flex-row align-items-center bad">
+                          <i className="flex-row align-items-center justify-content-center">{i !== pwordCheck.split('\n').length -1 && <MdCancel />}</i>{text}
+                        </span><br key={text} />
                       </>
                 )
               }
