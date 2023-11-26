7c9bddda5ffe024727fb48899a065c46498e50c2
Emmanuel Ezema <emmanuelezema6@gmail.com>
2023-11-27 00:56:36 +0100

    Made profile pic source to be dynamic

--- src/components/header_footer/Profile.jsx
+++ src/components/header_footer/Profile.jsx
@@ -1,16 +1,18 @@
 import { Link } from 'react-router-dom';
-import image from '../../assets/card-images/Sample_User_Icon.png';
+import { useMemo } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { logUserOut } from '../../state/features/userSlice';
+import { getProfilePic } from '../../utils/card';
 
 const Profile = () => {
-    const { username, profilePic = image } = useSelector(state => state.user);
+    const { username, profilePic } = useSelector(state => state.user);
+    const image = useMemo(() => getProfilePic(profilePic), [profilePic]);
     const dispatch = useDispatch();
     function logOut() { dispatch(logUserOut()) };
 
     return (
         <div className="profile relative">
-            <img src={profilePic} alt="User Image" />
+            <img src={image} alt="User Image" />
             <div className="profile_info absolute full-border flex-col jc-center ai-center">
                 <span className="username">{username}</span>
                 <Link to="/user/details" className="full-w profile-btn view-btn flex-row ai-center jc-center">View Profile</Link>
