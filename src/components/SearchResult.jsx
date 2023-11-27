300fe51740dbf4835ea6c7661a31542dffc81a71
Emmanuel Ezema <emmanuelezema6@gmail.com>
2023-11-27 01:02:59 +0100

    removed resolveCardImages function from component logic

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,11 +1,9 @@
 import { useMemo } from 'react';
 import CardsList from './CardsList';
 import { NothingFound } from './fetch_states';
-import { resolveCardImage } from '../utils/card';
 
 const SearchResult = ({ filterType, filter, cards }) => {
     const showCards = useMemo(() => cards.length > 0 && (filter === filterType || !filter), [cards, filterType, filter]);
-    const resolvedCards = useMemo(() => cards.map(resolveCardImage), [cards]);
 
     return (
         <div className="search_type">
@@ -14,7 +12,7 @@ const SearchResult = ({ filterType, filter, cards }) => {
                     <>
                         { !filter && <h3 className="dark_title">Result for Player {filterType}</h3> }
                         <div className={`search_result full-border ${filterType !== '' && 'give_margin'}`}>
-                            <CardsList cards={resolvedCards} />
+                            <CardsList cards={cards} />
                         </div>
                     </> :
                     filter === filterType ? <NothingFound text="No results found." /> : null
