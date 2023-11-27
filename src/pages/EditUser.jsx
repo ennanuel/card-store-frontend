f16e95e1f41b6ca68c2734dc07fbf61b726e6436
Emmanuel Ezema <emmanuelezema6@gmail.com>
2023-11-27 01:06:21 +0100

    Added profilePic and handleFileChange logic for input type 'file'

--- src/pages/EditUser.jsx
+++ src/pages/EditUser.jsx
@@ -1,17 +1,22 @@
 import { useState, useEffect } from 'react'
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { EditUserInfo } from '../components/forms';
+import { authenticateUser } from '../state/features/userSlice';
 import { useGetUserInfoQuery } from '../state/api';
 import { editUserInfo } from "../utils/user";
+import { getImageSrc } from '../utils';
+import image from "../assets/card-images/empty.jpg";
 import '../styles/user.scss';
 
 const EditUser = () => {
   const { _id } = useSelector(state => state.user);
   const { data = {}, isFetching, fetchError } = useGetUserInfoQuery(_id);
-  const [values, setValues] = useState({});
+  const [values, setValues] = useState({ first: '', middle: '', last: '', username: '', email: '', phone: '', dob: '', bank: '', account_number: '', profilePic: undefined });
+  const [imageSrc, setImageSrc] = useState('');
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -20,11 +25,20 @@ const EditUser = () => {
     setValues(prev => ({ ...prev, [name]: value }))
   };
 
+  const handleFileChange = async (e) => {
+    if (!e.target.files) return;
+    const file = e.target.files[0];
+    const imgSrc = await getImageSrc(file);
+    setImageSrc(imgSrc);
+    setValues(prev => ({ ...prev, profilePic: file }));
+  }
+
   const handleSubmit = async (e) => {
     try {
       e.preventDefault();
       setLoading(true);
       await editUserInfo(values, _id);
+      dispatch(authenticateUser());
       navigate('/user/details');
     } catch (error) {
       setError(true);
@@ -38,16 +52,20 @@ const EditUser = () => {
   }
     
   useEffect(() => {
-    const { names = {}, ...otherValues } = data;
-    setValues({ ...otherValues, ...names });
+    const { names = {}, dob = '', profilePic, ...otherValues } = data;
+    const dobWithoutTimeZone = dob?.replace(/T\d+:\d+:\d+.\d+Z/i, '');
+    setImageSrc(profilePic || image);
+    setValues({ ...otherValues, dob: dobWithoutTimeZone, ...names });
   }, [data]);
 
   return (
     <section className="user">
       <EditUserInfo
         userDetails={values}
+        imageSrc={imageSrc}
         loading={loading || isFetching}
         error={error || fetchError}
+        handleFileChange={handleFileChange}
         handleChange={handleChange}
         handleSubmit={handleSubmit}
         retry={retry}
