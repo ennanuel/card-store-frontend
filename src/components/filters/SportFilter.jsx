4d1deda244b9560069e5c82f8ecf80b32977e6e4
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-01-14 21:26:31 +0100

    Refactored components to utilize redux 'useGetQuery' and also implemented new 'Team' and 'Sport' filter components in each component

--- src/components/filters/SportFilter.jsx
+++ src/components/filters/SportFilter.jsx
@@ -1,32 +1,30 @@
-import { useState, useEffect, useMemo } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { fetchSports } from '../../utils/card';
+import { useState, useMemo, useRef } from 'react';
+import { useGetSportsQuery } from '../../state/api';
+import Options from './options';
 
 const SportFilter = ({ searchValue = 'All' }) => {
-    const [sports, setSports] = useState([]);
-    const sportName = useMemo(() => searchValue.replace('+', ' '), [searchValue])
-    const navigate = useNavigate();
+    const limit = useMemo(() => 6, []);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [search, setSearch] = useState('');
 
-    const navigateToSport = (e) => {
-        const sportName = e.target.value;
-        navigate(`/cards/sport/${sportName.replace(' ', '+')}`);
-    }
-
-    useEffect( () => {
-        fetchSports()
-            .then(res => setSports(res))
-            .catch(error => console.error(error));
-    }, [])
+    const { data, isFetching, error } = useGetSportsQuery({ limit, page: currentPage, search });
+    const { sports, totalPages } = useMemo(() => data || { sports: [], totalPages: 0 }, [data]);
     
     return (
-        <article className="sport_filter flex-row ai-center">
-            <label htmlFor="select_team">Showing <b className="highlight">{sportName}</b> Cards</label>
-            <select name="select_team" className="full-w" id="select_team" onChange={navigateToSport}>
-                <option value="all"> All Sports </option>
-                { sports.map( ({ name }, i) => <option key={i} value={name}>{name}</option>) }
-            </select>
-        </article>
+        <Options
+            searchValue={searchValue}
+            options={sports}
+            link={'/cards/sport'}
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+            totalPages={totalPages}
+            loading={isFetching}
+            error={error}
+            search={search}
+            isTeamFilter={true}
+            setSearch={setSearch}
+        />
     )
-}
+};
 
 export default SportFilter
--- src/components/filters/TeamFilter.jsx
+++ src/components/filters/TeamFilter.jsx
@@ -1,33 +1,29 @@
-import { useState, useEffect, useMemo } from "react";
-import { fetchTeams } from "../../utils/card";
-import { useNavigate } from "react-router-dom";
+import { useState, useMemo } from "react";
+import { useGetTeamsQuery } from "../../state/api";
+import Options from "./options";
 
-const TeamFilter = ({ searchValue = 'All Teams' }) => {
-    const navigate = useNavigate();
-    const [teams, setTeams] = useState([]);
-    const teamName = useMemo(() => searchValue.replace('+', ' '), [searchValue])
-    const navigateToTeam = (e) => {
-        const teamName = e.target.value;
-        navigate(`/cards/team/${teamName.replace(' ', '+')}`);
-    }
+const TeamFilter = ({ searchValue = 'All' }) => {
+    const limit = useMemo(() => 6, []);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [search, setSearch] = useState('');
 
-    useEffect( () => {
-        fetchTeams()
-            .then(res => setTeams(res))
-            .catch(error => console.error(error));
-    }, [])
+    const { data, isFetching, error } = useGetTeamsQuery({ limit, page: currentPage, search });
+    const { teams, totalPages } = useMemo(() => data || { teams: [], totalPages: 0 }, [data]);
     
     return (
-        <article className="sport_filter flex-row ai-center">
-            <label htmlFor="select_team">
-                <span>Showing cards of players in </span>
-                <b className="highlight">{teamName}</b>
-            </label>
-            <select name="select_team" id="select_team" onChange={navigateToTeam}>
-                <option value="all"> All Team </option>
-                { teams.map( (team, i) => <option key={i} value={team.name}>{team.name} ({team.sport})</option>) }
-            </select>
-        </article>
+        <Options
+            searchValue={searchValue}
+            options={teams}
+            link={'/cards/team'}
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+            totalPages={totalPages}
+            loading={isFetching}
+            error={error}
+            search={search}
+            isTeamFilter={true}
+            setSearch={setSearch}
+        />
     )
 }
 
